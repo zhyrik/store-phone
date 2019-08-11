b2e9d19c3c55432f7105cdd8ae9861c43130f7e7
zhurik <zhyriki@ukr.net>
2019-08-11 23:52:19 +0300

    create functions to cart & details

--- src/App.js
+++ src/App.js
@@ -15,12 +15,48 @@ import { storeProducts, detailProduct } from './data'
 
 function App() {
   const [product, setProduct] = useState(storeProducts)
+  const [detailProduct, setDetailsProduct] = useState({})
+  const [cart, setCart] = useState([])
+  const [modelOpen, setModelOpen] = useState(true)
+  const [modelProduct, setModelProduct] = useState(detailProduct)
+  
   useEffect(() => {
     const newProduct = JSON.parse(JSON.stringify(product))
     setProduct(newProduct)
   }, [])
+
+  // get one product from array 
+  const getItem = id => {
+    const productItem = product.find(item => item.id === id)
+    return productItem
+  }
+  // put our one product on details
+  const handleDetail = id => {
+    const productItem = getItem(id)
+    setDetailsProduct(productItem)
+  }
+
+  // add product to cart
+  const addToCart = id => {
+    let tempProducts = [...product] // warning bug (array not rewriting)
+    const index = tempProducts.indexOf(getItem(id))
+    const productItem = tempProducts[index]
+    productItem.inCart = true
+    productItem.count = 1
+    const price = productItem.price
+    productItem.total = price
+
+    // setProduct(productItem)
+    setCart([...cart, productItem])
+    console.log(product, cart)
+  }
+
+   const openModel => {
+
+  }
+
   return (
-    <AppContext.Provider value={{product, detailProduct }}>
+    <AppContext.Provider value={{product, detailProduct, handleDetail, addToCart }}>
       <Navbar />
       <Switch>
         <Route exact path="/" component={ProductList} />
--- src/components/Product.js
+++ src/components/Product.js
@@ -7,17 +7,17 @@ import PropTypes from 'prop-types'
 import { AppContext } from './Context'
 
 function Product({ product }) {
-  const { title, img, price, inCart } = product
-  const { } = useContext(AppContext)
+  const { id, title, img, price, inCart } = product
+  const { handleDetail, addToCart } = useContext(AppContext)
   return (
 
     <Wrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
       <div className="card">
-        <div className="img-container p-5" onClick={() => console.log('prod')}>
+        <div className="img-container p-5" onClick={() => handleDetail(id)}>
           <Link to="/details">
             <img src={img} alt="product" className="card-img-top"/>
           </Link>
-          <button className="card-btn" disabled={inCart?true:false} onClick={()=> console.log('added')}>
+          <button className="card-btn" disabled={inCart?true:false} onClick={()=> addToCart(id)}>
             {inCart? (<p className="text-capitalize mb-0" disabled>
               in Cart
             </p>): <FaCartPlus />}
